b58ee6e176bf465d5f40e08c2e917ae0bae02004
Shai Mann-Robison <mann.robison@gmail.com>
2025-03-23 20:55:58 -0400

    create basic API

--- server/app.py
+++ server/app.py
@@ -13,5 +13,22 @@ def get_planet():
     return {"id": planet_id}
 
 
+# define a GET endpoint for the /planets route, which will return a list of planets
+# that the user has already seen and voted on.
+@app.route("/planets", methods=["GET"])
+def get_planets():
+    # for now, generate a random list of planet IDs. In the future, this will be real planet data.
+    planet_ids = [random.randint(0, 1000000) for _ in range(10)]
+    # send the data back to the client
+    return {"ids": planet_ids}
+
+
+# define a POST endpoint for the /planet route, which will allow the user to vote on a planet
+@app.route("/vote/<planet_id>", methods=["POST"])
+def vote_on_planet(planet_id):
+    # send the data back to the client
+    return {"id": planet_id}
+
+
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=8080)
